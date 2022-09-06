073981ec6923ec3bf274114c99094dbbbe246079
Medeiroshenrique <henrique.c.r.medeiros@gmail.com>
2022-09-06 19:02:41 -0300

    como tudo comecou

---
+++ Projeto.cs
@@ -0,0 +1,29 @@
+﻿public class Projeto
+{
+    private int _id;
+    private string _name;
+    private string _status;
+    private float _total;
+    private DateTime _dataCriacao;
+    private DateTime _prazo;
+
+    public int Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+    public string Status {
+        get { return _status; }
+        set { _status = value; }
+    }
+    public float Total {
+        get { return _total; }
+        set { _total = value; }
+    }
+
+}
