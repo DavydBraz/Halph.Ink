d9eeca3913dc00bd5fc9be08b43cc4f420db9de7
Henrique  Medeiros <102472768+Medeiroshenrique@users.noreply.github.com>
2022-10-03 16:18:06 -0300

    Update Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -1,6 +1,19 @@
+using System.Collections;
+
 namespace Halph.Ink;
 
 public class Cliente:Pessoa
 {
-    
+    private ArrayList _projeto = new ArrayList();
+
+    public void AdicionarProjeto(Projeto projeto)
+    {
+        _projeto.Add(projeto);
+    }
+    public void RemoverProjeto(Projeto projeto)
+    {
+        _projeto.Remove(projeto);
+    }
+
+
 }
