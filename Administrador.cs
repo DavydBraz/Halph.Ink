0e122ef6d913865259ea296bf674f898904bd596
Henrique  Medeiros <102472768+Medeiroshenrique@users.noreply.github.com>
2022-10-03 16:17:40 -0300

    Update Administrador.cs

--- Administrador.cs
+++ Administrador.cs
@@ -1,6 +1,15 @@
 namespace Halph.Ink;
-
+using System.Collections;
 public class Administrador:Pessoa
 {
+    private Projeto[] _projetos;
+    private Cliente[] _clientes;
+    private Designer[] _designers;
     
+    public void VinculaEntidades(){}
+    public void DesvinculaEntidades(){}
+    public void EditaServicosProjeto(){}
+    public void EditaStatusProjeto(){}
+    public void PagaDesigner(){}
+    public void ReembolsaCliente(){}
 }
