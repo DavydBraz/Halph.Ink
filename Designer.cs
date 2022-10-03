5eb8d5bac2afe4fbead4db2377618615efb24dde
Henrique  Medeiros <102472768+Medeiroshenrique@users.noreply.github.com>
2022-10-03 16:17:02 -0300

    Update Designer.cs

--- Designer.cs
+++ Designer.cs
@@ -1,6 +1,10 @@
+using System.Collections;
 namespace Halph.Ink;
 
 public class Designer
 {
-    
+    private Formacao[] _formacao;
+    private float _comissao;
+    private string _status;
+    private Projeto[] _projeto;
 }
